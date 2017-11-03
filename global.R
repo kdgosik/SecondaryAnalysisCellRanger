a68b3c3ec3b6dd71d7fa924a0eb2d0da1e44e51a
kgosik <kgosik@hw-uger-1004.broadinstitute.org>
2017-11-03 12:05:42 -0400

    Corrected global.R file

--- global.R
+++ global.R
@@ -3,9 +3,9 @@ if( !{"cellrangerRkit" %in% installed.packages()} ) {
   source("http://cf.10xgenomics.com/supp/cell-exp/rkit-install-2.0.0.R")
 }
 
-pkgs <- c("shiny", "shinyFiles", "shinycssloader", "cellranger", "Seurat", 
+pkgs <- c("shiny", "shinyFiles", "shinycssloaders", "cellranger", "Seurat", 
           "ggplot2", "plotly", "dplyr", "Matrix")
-pkgs <- pkgs[pkgs %in% installed.packages()[,1]]
+pkgs <- pkgs[!{pkgs %in% installed.packages()[,1]}]
 
 if( !{all(pkgs %in% installed.packages()[,1])} ) install.packages(pkgs)
 
